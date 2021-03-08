9e724208a4b6b12af16cbc31caff330d2dec5c8e
Vitaly <hellvesper@gmail.com>
2021-03-08 13:35:26 +0200

    Update client.py
    Add 500 status code to retry

--- notion/client.py
+++ notion/client.py
@@ -40,7 +40,7 @@ def create_session(client_specified_retry=None):
         retry = Retry(
             5,
             backoff_factor=0.3,
-            status_forcelist=(502, 503, 504),
+            status_forcelist=(500, 502, 503, 504),
             # CAUTION: adding 'POST' to this list which is not technically idempotent
             method_whitelist=(
                 "POST",
